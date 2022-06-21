d0195c71cc6fb50b66849e7e0f1da24b05ab25d5
Wolfgang <mail@weirdrescue.pw>
2022-06-21 14:39:34 +0200

    Edit the bootstrapping script

--- prepare_system.sh
+++ prepare_system.sh
@@ -2,11 +2,10 @@
 # A bash script that prepares the OS
 # before running the Ansible playbook
 
-passwd &&
-apt update -y
-apt-get -o Dpkg::Options::="--force-confold" -fuy dist-upgrade
-apt-get -o Dpkg::Options::="--force-confold" -fuy install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y
-[ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python3-dev libffi-dev libssl-dev make -y
-pip3 install ansible -U
-git clone https://github.com/notthebee/ansible-easy-vpn 
+apt update -y &&
+apt-get -o Dpkg::Options::="--force-confold" -fuy dist-upgrade &&
+apt-get -o Dpkg::Options::="--force-confold" -fuy install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y &&
+[ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python3-dev libffi-dev libssl-dev make -y &&
+pip3 install ansible -U &&
+git clone https://github.com/notthebee/ansible-easy-vpn &&
 reboot
