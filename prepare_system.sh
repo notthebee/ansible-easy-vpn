20dd6c4d4f102355e7a0baa49f7e07243ada5745
Wolfgang <mail@weirdrescue.pw>
2022-06-21 15:56:20 +0200

    Attempt to fix env

--- prepare_system.sh
+++ prepare_system.sh
@@ -4,16 +4,17 @@
 
 # root or not
 if [[ $EUID -ne 0 ]]; then
-  SUDO='sudo -H'
+  SUDO='sudo -H -E'
 else
   SUDO=''
 fi
 
-ENV='DEBIAN_FRONTEND=noninteractive'
-$SUDO $ENV apt update -y;
-$SUDO $ENV yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq dist-upgrade;
-$SUDO $ENV yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y;
-[ $(uname -m) == "aarch64" ] && $SUDO $ENV yes | apt install gcc python3-dev libffi-dev libssl-dev make -yqq;
+export DEBIAN_FRONTEND=noninteractive
+$SUDO apt update -y;
+$SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq dist-upgrade;
+$SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y;
+[ $(uname -m) == "aarch64" ] && $SUDO yes | apt install gcc python3-dev libffi-dev libssl-dev make -yqq;
 
+export DEBIAN_FRONTEND=
 $SUDO pip3 install ansible -U &&
 [ -d "$HOME/ansible-easy-vpn" ] || git clone https://github.com/notthebee/ansible-easy-vpn
