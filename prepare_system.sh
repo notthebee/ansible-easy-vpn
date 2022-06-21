144dba6bf42a5f7bde06db2c6a1a494866fd5bc0
Wolfgang <mail@weirdrescue.pw>
2022-06-21 15:52:46 +0200

    Add sudo support

--- prepare_system.sh
+++ prepare_system.sh
@@ -2,12 +2,18 @@
 # A bash script that prepares the OS
 # before running the Ansible playbook
 
-export DEBIAN_FRONTEND=noninteractive
-apt update -y;
-yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq dist-upgrade;
-yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq install software-properties-common curl git facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y;
-[ $(uname -m) == "aarch64" ] && yes | apt install gcc python3-dev libffi-dev libssl-dev make -yqq;
+# root or not
+if [[ $EUID -ne 0 ]]; then
+  SUDO='DEBIAN_FRONTEND=noninteractive sudo -H'
+else
+  SUDO='DEBIAN_FRONTEND=noninteractive'
+fi
+
+$SUDO apt update -y;
+$SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq dist-upgrade;
+$SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y;
+[ $(uname -m) == "aarch64" ] && $SUDO yes | apt install gcc python3-dev libffi-dev libssl-dev make -yqq;
 
 export DEBIAN_FRONTEND=
-pip3 install ansible -U &&
-[ -d "/root/ansible-easy-vpn" ] || git clone https://github.com/notthebee/ansible-easy-vpn
+$SUDO pip3 install ansible -U &&
+[ -d "$HOME/ansible-easy-vpn" ] || git clone https://github.com/notthebee/ansible-easy-vpn
