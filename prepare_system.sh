effc826e098e4aab081a00a2557d66e21ec0d63d
Wolfgang <mail@weirdrescue.pw>
2022-06-21 15:27:28 +0200

    Idempotence fixes

--- prepare_system.sh
+++ prepare_system.sh
@@ -5,10 +5,9 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update -y;
 yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq dist-upgrade;
-yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y;
+yes | apt-get -o Dpkg::Options::="--force-confold" -fuyqq install software-properties-common curl git facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y;
 [ $(uname -m) == "aarch64" ] && yes | apt install gcc python3-dev libffi-dev libssl-dev make -yqq;
 
 export DEBIAN_FRONTEND=
 pip3 install ansible -U &&
-git clone https://github.com/notthebee/ansible-easy-vpn &&
-reboot
+[ -d "/root/ansible-easy-vpn" ] || git clone https://github.com/notthebee/ansible-easy-vpn
