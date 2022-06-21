ced02199ebbfa5eb78ea74bcbca2d4b5a3f64763
Wolfgang <mail@weirdrescue.pw>
2022-06-21 14:32:27 +0200

    Edit the bootstrapping script

--- prepare_system.sh
+++ prepare_system.sh
@@ -8,3 +8,5 @@ apt-get -o Dpkg::Options::="--force-confold" -fuy dist-upgrade
 apt-get -o Dpkg::Options::="--force-confold" -fuy install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y
 [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python3-dev libffi-dev libssl-dev make -y
 pip3 install ansible -U
+git clone https://github.com/notthebee/ansible-easy-vpn 
+reboot
