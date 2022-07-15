0ba24033c3da83dab2570a2524439fac71ed1422
Wolfgang <mail@weirdrescue.pw>
2022-07-15 09:52:36 +0200

    Put pip back

--- prepare_system.sh
+++ prepare_system.sh
@@ -12,9 +12,10 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 $SUDO apt update -y;
 $SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuy dist-upgrade;
-$SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuy install software-properties-common curl git python3 python3-setuptools python3-apt python3-pip ansible python3-passlib python3-wheel python3-bcrypt aptitude -y;
+$SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuy install software-properties-common curl git python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y;
+$SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuy autoremove;
 [ $(uname -m) == "aarch64" ] && $SUDO yes | apt install gcc python3-dev libffi-dev libssl-dev make -y;
 
+pip3 install ansible
 export DEBIAN_FRONTEND=
-export EDITOR=nano
 [ -d "$HOME/ansible-easy-vpn" ] || git clone https://github.com/notthebee/ansible-easy-vpn
