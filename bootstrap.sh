b617bfc6f4af556bf2d7bb4ecfc3400defacccad
Wolfgang <mail@weirdrescue.pw>
2022-07-24 19:58:16 +0200

    Always install dnsutils

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,7 +38,7 @@ export DEBIAN_FRONTEND=noninteractive
 # Update apt database, update all packages and install Ansible + dependencies
 $SUDO apt update -y;
 $SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuy dist-upgrade;
-$SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuy install software-properties-common curl git python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y;
+$SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuy install software-properties-common dnsutils curl git python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y;
 $SUDO yes | apt-get -o Dpkg::Options::="--force-confold" -fuy autoremove;
 [ $(uname -m) == "aarch64" ] && $SUDO yes | apt install gcc dnsutils python3-dev libffi-dev libssl-dev make -y;
 
