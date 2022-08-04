321c24dc348a6d84f95cefe8fd1bab528d9d7d93
in2gee <109985970+in2gee@users.noreply.github.com>
2022-08-04 19:39:05 +0300

    Update bootstrap.sh
    Let's return the variable $os to line 13 so that line 22 will work correctly

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,18 +10,19 @@ set -e
 
 # Detect OS
 if grep -qs "ubuntu" /etc/os-release; then
-	os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+  os="ubuntu"
+  os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
 else
-	echo "This installer seems to be running on an unsupported distribution.
-Supported distros are Ubuntu 20.04 and 22.04"
-	exit
+  echo "This installer seems to be running on an unsupported distribution."
+  echo "Supported distros are Ubuntu 20.04 and 22.04"
+  exit
 fi
 
 # Check if the Ubuntu version is too old
 if [[ "$os" == "ubuntu" && "$os_version" -lt 2004 ]]; then
-	echo "Ubuntu 20.04 or higher is required to use this installer.
-This version of Ubuntu is too old and unsupported."
-	exit
+  echo "Ubuntu 20.04 or higher is required to use this installer."
+  echo "This version of Ubuntu is too old and unsupported."
+  exit
 fi
 
 
