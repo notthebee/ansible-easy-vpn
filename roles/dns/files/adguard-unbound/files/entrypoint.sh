4c1e292e18ebba70efbe67570de0d2b16bae8570
Wolfgang <mail@weirdrescue.pw>
2022-11-20 18:37:45 +0100

    Sort out entrypoint and other things

--- roles/dns/files/adguard-unbound/files/entrypoint.sh
+++ roles/dns/files/adguard-unbound/files/entrypoint.sh
@@ -1,25 +1,6 @@
 #!/bin/ash
-
-
 unbound -c /opt/unbound/unbound.conf
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start unbound: $status"
-  exit $status
-fi
 
-/opt/adguardhome/AdGuardHome -c /opt/adguardhome/conf/AdGuardHome.yaml -w /opt/adguardhome/work --no-check-update
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start AdGuardHome: $status"
-  exit $status
-fi
+nohup /opt/adguardhome/AdGuardHome -c /opt/adguardhome/conf/AdGuardHome.yaml -w /opt/adguardhome/work --no-check-update &
 
 dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start dnscrypt-proxy: $status"
-  exit $status
-fi
-
-
