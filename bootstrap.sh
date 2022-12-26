efda12b2045ada5cd2fe7fb1b270fdd3fff7234d
Wolfgang <mail@weirdrescue.pw>
2022-12-26 09:52:15 +0100

    [skip ci] Bump ansible to 7.1

--- bootstrap.sh
+++ bootstrap.sh
@@ -87,7 +87,7 @@ yes | $SUDO apt-get -o Dpkg::Options::="--force-confold" -fuy autoremove;
 
 check_root "-H"
 
-$SUDO pip3 install ansible~=6.2 &&
+$SUDO pip3 install ansible~=7.1 &&
 export DEBIAN_FRONTEND=
 
 check_root
