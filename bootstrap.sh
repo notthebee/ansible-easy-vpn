2626c56587b2e0c0ec01413505877ceabeac1ce8
Wolfgang <mail@weirdrescue.pw>
2023-01-02 22:40:26 +0100

    Pin cryptography to 36.0.2 and pyOpenSSL to 20.0.1 (#114)

--- bootstrap.sh
+++ bootstrap.sh
@@ -87,6 +87,7 @@ yes | $SUDO apt-get -o Dpkg::Options::="--force-confold" -fuy autoremove;
 
 check_root "-H"
 
+$SUDO pip3 install "cryptography<=36.0.2" "pyOpenSSL<=20.0.1"
 $SUDO pip3 install ansible~=7.1 || $SUDO pip3 install ansible
 export DEBIAN_FRONTEND=
 
