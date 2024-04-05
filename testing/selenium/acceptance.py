5d674d0619d2ebd4eca6ed108304ca71bd9c91ee
Wolfgang <mail@weirdrescue.pw>
2024-04-05 13:45:38 +0200

    Modify testing

--- testing/selenium/acceptance.py
+++ testing/selenium/acceptance.py
@@ -2,8 +2,12 @@
 
 from pexpect import pxssh
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
@@ -14,7 +18,8 @@ import pyotp
 import re
 
 
-service = Service(executable_path=r"/snap/bin/chromium.chromedriver")
+service = Service(executable_path=r"/opt/homebrew/bin/chromedriver")
+#service = Service(executable_path=r"/snap/bin/chromium.chromedriver")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--username", type=str, metavar="username")
@@ -26,9 +31,10 @@ args = parser.parse_args()
 
 chrome_options = Options()
 prefs = {"download.default_directory": "/home/runner"}
+#prefs = {"download.default_directory": "/Users/notthebee/Downloads"}
 chrome_options.add_experimental_option("prefs", prefs)
 options = [
-    "--headless",
+    #"--headless",
     "--disable-gpu",
     "--window-size=1920,1200",
     "--ignore-certificate-errors",
@@ -46,64 +52,57 @@ logging.basicConfig()
 logger.setLevel(logging.DEBUG)
 
 
+def save_screenshot(screenshot_name):
+    screenshot_path = "/home/runner/screenshots/"
+    #screenshot_path = "/Users/notthebee/Downloads/"
+    driver.save_screenshot(screenshot_path + screenshot_name)
+    return
+
 def register_2fa(driver, base_url, username, password, ssh_agent):
     logger.debug(f"Fetching wg.{base_url}")
     driver.get(f"https://wg.{base_url}")
-    sleep(2)
+
     logger.debug(f"Filling out the username field with {username}")
-    username_field = driver.find_element("id", "username-textfield")
-    username_field.send_keys(username)
-    sleep(0.5)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "username-textfield"))).send_keys(username)
+    save_screenshot("1_AutheliaUsername.png")
     logger.debug(f"Filling out the password field with {password}")
-    password_field = driver.find_element("id", "password-textfield")
-    password_field.send_keys(password)
-    sleep(0.5)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "password-textfield"))).send_keys(password)
+    save_screenshot("2_AutheliaPassword.png")
+
     logger.debug("Signing in...")
-    submit_button = driver.find_element("id", "sign-in-button")
-    submit_button.click()
-    sleep(5)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "sign-in-button"))).click()
 
     logger.debug("Clicking on 'Register device'")
-    register_device = driver.find_element("id", "register-link")
-    register_device.click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "register-link"))).click()
+    save_screenshot("3_RegisterDevice.png")
 
-    sleep(2)
     logger.debug("Clicking on 'One-Time Password'")
-    one_time_password_add = driver.find_element("id", "one-time-password-add")
-    one_time_password_add.click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "one-time-password-add"))).click()
+    save_screenshot("4_OTP.png")
 
     logger.debug("Getting the notifications.txt from the server")
-
     s = pxssh.pxssh(options={"IdentityAgent": ssh_agent})
     s.login(base_url, username)
     s.sendline("show_2fa")
     s.prompt()
-
     # Convert output to utf-8 due to pexpect weirdness
     notification = "\r\n".join(s.before.decode("utf-8").splitlines()[1:])
     print(notification)
-
     token = re.search("single-use code: (.*)", notification).group(1)
 
-    one_time_password = driver.find_element("id", "one-time-code")
-    one_time_password.click()
-    sleep(1)
+    logger.debug("Entering the one-time code")
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "one-time-code"))).click()
+    save_screenshot("5_EnteringOTP.png")
 
     actions = ActionChains(driver)
     actions.send_keys(token)
     actions.perform()
 
-    sleep(1)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "dialog-verify"))).click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "dialog-next"))).click()
 
-    verify_button = driver.find_element("id", "dialog-verify")
-    verify_button.click()
-
-    sleep(3)
-
-    next_button = driver.find_element("id", "dialog-next")
-    next_button.click()
-
-    sleep(3)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "secret-url")))
+    save_screenshot("6_SecetURL.png")
 
     secret_field = driver.find_element("id", "secret-url")
     secret_field = secret_field.get_attribute("value")
@@ -115,8 +114,7 @@ def register_2fa(driver, base_url, username, password, ssh_agent):
     totp.now()
     logger.debug("Generating the OTP")
 
-    next_button = driver.find_element("id", "dialog-next")
-    next_button.click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "dialog-next"))).click()
 
     sleep(3)
 
@@ -134,42 +132,40 @@ def register_2fa(driver, base_url, username, password, ssh_agent):
 def download_wg_config(driver, base_url, client, secret):
     logger.debug(f"Opening wg.{base_url} in the browser")
     driver.get(f"https://wg.{base_url}")
-    sleep(2)
 
+    save_screenshot("7_AutheliaOTPEnter.png")
     totp = pyotp.TOTP(secret)
 
-    actions = ActionChains(driver)
-    actions.send_keys(totp.now())
-    actions.perform()
-
-    sleep(1)
-
-    # Sometimes Authelia does not accept the TOTP code the first time...
-    actions = ActionChains(driver)
-    actions.send_keys(totp.now())
-    actions.perform()
-
-    sleep(1)
-
-    logger.debug(f"Opening wg.{base_url} in the browser")
-    driver.get(f"https://wg.{base_url}")
-    sleep(5)
 
     logger.debug("Clicking on the 'New Client' button")
-    new_client_button = driver.find_element("xpath", "//*[contains(text(), 'New Client')]")
-    new_client_button.click()
-    sleep(2)
+
+    attempts = 0
+    while attempts < 5:
+        try:
+            actions = ActionChains(driver)
+            actions.send_keys(totp.now())
+            actions.perform()
+
+            logger.debug(f"Opening wg.{base_url} in the browser")
+            driver.get(f"https://wg.{base_url}")
+
+            WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'New Client')]"))).click()
+            save_screenshot("8_WGEeasy_NewClient.png")
+            break
+        except TimeoutException:
+            attempts += 1
+
     logger.debug(f"Filling out the 'Name' field with {client}")
-    name_field = driver.find_element("xpath", "//input[@placeholder='Name']")
-    name_field.send_keys(client)
-    sleep(2)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Name']"))).send_keys(client)
+    save_screenshot("9_WGEeasy_NewClientName.png")
+
     logger.debug("Clicking on 'Create'")
-    create_button = driver.find_element("xpath", "//*[contains(text(), 'Create')]")
-    create_button.click()
-    sleep(2)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Create')]"))).click()
+
+    save_screenshot("10_WGEeasy_ClientCreated.png")
     logger.debug("Downloading the configuration")
-    download_config = driver.find_element("xpath", "//a[@title='Download Configuration']")
-    download_config.click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[@title='Download Configuration']"))).click()
+    sleep(2)
 
     return
 
